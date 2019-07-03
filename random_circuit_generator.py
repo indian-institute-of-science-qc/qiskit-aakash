9f09f68a55709a336cba51309a75c8ede97194af
Naman Roshan <namanroshan12@gmail.com>
2019-07-04 00:29:23 +0530

    checking made easy

--- random_circuit_generator.py
+++ random_circuit_generator.py
@@ -1,6 +1,16 @@
 import numpy as np 
 import random
 from random import randint
+
+print('from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister')
+print('from qiskit import BasicAer, execute')
+print("backend1 = BasicAer.get_backend('dm_simulator')") 
+print("backend2 = BasicAer.get_backend('qasm_simulator')")
+print('options = {}')
+print('q = QuantumRegister(4)')
+print('c = ClassicalRegister(4)')
+print('qc = QuantumCircuit(q, c)')
+
 noq = 4
 l = ['u1','u2','u3','cx','ccx']
 for i in range(random.randint(20,1000)):
@@ -21,6 +31,14 @@ for i in range(random.randint(20,1000)):
     elif x == 4:
         a, b, c = random.sample(range(noq),3)
         print('qc.{}(q[{}],q[{}],q[{}])'.format(l[x], a, b, c))
+
+print('circuits = [qc]')
+print('job = execute(circuits, backend1, **options)')
+print('result = job.result()')
+print('print(result)')  
+print('job = execute(circuits, backend2, **options)')
+print('result = job.result()')
+print('print(result)')      
 
 
 
