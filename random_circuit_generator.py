6aac03709e0a4a8820cbf793bd43de6dfc31ed1a
Naman Roshan <namanroshan12@gmail.com>
2019-07-03 23:20:59 +0530

    rcg2

--- random_circuit_generator.py
+++ random_circuit_generator.py
@@ -13,7 +13,7 @@ for i in range(random.randint(1,1000)):
          print('qc.{}({},{},{},q[{}])'.format(l[x],random.randint(1,100),random.randint(1,100),random.randint(1,100),random.randint(0,noq-1)))
     elif x == 3:
         print('qc.{}(q[{}],q[{}])'.format(l[x],random.randint(0,noq-1),random.randint(0,noq-1)))
-        break
+        
     elif x == 4:
         print('qc.{}(q[{}],q[{}],q[{}])'.format(l[x],random.randint(0,noq-1),random.randint(0,noq-1),random.randint(0,noq-1)))
 
