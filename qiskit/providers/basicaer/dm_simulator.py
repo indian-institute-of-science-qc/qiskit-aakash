583c5df1ae636dea0d3161b8eb9e61e789c8f49e
sanju37 <biplabmahato37@gmail.com>
2019-07-13 14:23:14 +0530

    Reset updated to density matrix formalism

--- qiskit/providers/basicaer/dm_simulator.py
+++ qiskit/providers/basicaer/dm_simulator.py
@@ -600,19 +600,24 @@ class DmSimulatorPy(BaseBackend):
         outcome and projecting onto the outcome state while
         renormalizing.
         """
-        # get measure outcome
-        outcome, probability = self._get_measure_outcome(qubit)
-        # update quantum state
 
-        if outcome == '0':
-            update = 1/np.sqrt(probability)*np.array(
-                [[1,0,0,0],[0,0,0,0],[0,0,0,0],[1,0,0,0]], dtype=float)
-            self._add_unitary_single(update, qubit)
-        else:
-            update = 1/np.sqrt(probability)*np.array(
-                [[1,0,0,-1], [0,1,0,0], [0,0,1,0], [1,0,0,-1]], dtype=float)
-        # update classical state
-            self._add_unitary_single(update, qubit)
+        # update density matrix
+        self._densitymatrix =  np.reshape(self._densitymatrix,(4**(qubit),4,4**(self._number_of_qubits-qubit-1)))
+
+        self._densitymatrix[:,0,:] += self._densitymatrix[:,3,:]
+        self._densitymatrix[:,1,:] = 0
+        self._densitymatrix[:,2,:] = 0
+        self._densitymatrix[:,3,:] = self._densitymatrix[:,0,:]
+
+        membit = 1 << cmembit
+        self._classical_memory = (self._classical_memory & (
+            ~membit)) | (int(outcome) << cmembit)
+
+        if cregbit is not None:
+            regbit = 1 << cregbit
+            self._classical_register = \
+                (self._classical_register & (~regbit)) | (
+                    int(outcome) << cregbit)
 
     def _validate_initial_densitymatrix(self):
         """Validate an initial densitymatrix"""
