5613c2e97a45edd1039e82474350697fb923a589
biplab37 <40288287+biplab37@users.noreply.github.com>
2019-08-14 18:54:13 +0530

    added matplolib and pillow into setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,9 @@ REQUIREMENTS = [
     "ply>=3.10",
     "psutil>=5",
     "scipy>=1.0",
-    "sympy>=1.3"
+    "sympy>=1.3",
+    "matplotlib>=1.5",
+    "pillow>=6.0"
 ]
 
 # Add Cython extensions here
