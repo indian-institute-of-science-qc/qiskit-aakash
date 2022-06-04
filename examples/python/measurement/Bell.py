2bd3893f1f0e55e172ef35803328f4103a08ab0c
SamarthH <samarth.hawaldar@gmail.com>
2022-06-04 15:53:31 +0530

    Updated Examples

--- examples/python/measurement/Bell.py
+++ examples/python/measurement/Bell.py
@@ -9,5 +9,5 @@ options2 = {
 backend = BasicAer.get_backend('dm_simulator')
 run2 = execute(qc2,backend,**options2)
 result2 = run2.result()
-result2['results'][0]['data']['bell_probabilities01']
+print(result2['results'][0]['data']['bell_probabilities01'])
 print('Density Matrix: \n',result2['results'][0]['data']['densitymatrix'])
